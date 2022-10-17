f53e64af1aa3960cd264eaa4c58e163af0a7e207
AWS ParallelCluster user <ec2-user@ip-10-0-7-144.ap-southeast-1.compute.internal>
2022-10-17 03:17:53 +0000

    Add my copies

---
+++ myec2snapshot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ssh -o StrictHostKeyChecking=no -i /data/MyKeyPair.pem ec2-user@3.1.51.102 "source ~/.bash_profile; pcluster update-compute-fleet --status STOP_REQUESTED --region ap-southeast-1 --cluster-name MyCluster01; ~/update_snapshot.sh data 2 MyCluster01"
+
